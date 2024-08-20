84633c40e1ff7cb95a14e9bd452c55b0c064cefc
tanmay-kreeda <tanmay.shimpi@kreedalabs.com>
2024-08-20 12:28:20 +0530

    Added variable

---
+++ 03_variables/variables.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// once declared you have to use variables in golang
+	// var name string = "golang"
+
+	// infer => Golang can infer type of variable based on its value
+	// var firstName = "Tanmay"
+	// var age int = 30
+
+	// fmt.Println(name)
+	// fmt.Println(firstName)
+	// fmt.Println(age)
+
+	// shorthand syntax
+	lastName := "golang" // used when we are declaring and assigning a variable at the same time
+	fmt.Println(lastName)
+
+	// to declare variable without assigning values you must use this syntax
+	// var name string
+	// name = "Tanmay"
+
+	// var price float32
+	// price = 50.5
+	// var price = 50.5
+	price := 50.5
+
+	fmt.Println(price)
+
+}
