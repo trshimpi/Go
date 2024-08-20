690bcc8c3e8748c4f82612b387551f75373d8f88
tanmay-kreeda <tanmay.shimpi@kreedalabs.com>
2024-08-20 12:18:42 +0530

    Added simple values

---
+++ 02_simple_values/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// simple values
+	//int
+	fmt.Println(1)
+
+	//string
+	fmt.Println("Tanmay")
+
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//floats
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
+
+}
