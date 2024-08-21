bdbf2c9e830769eea6dbb13c95eae5d19888c475
tanmay-kreeda <tanmay.shimpi@kreedalabs.com>
2024-08-21 22:42:49 +0530

    Added range

---
+++ 11_range/range.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nums := []int{1, 2, 3, 4}
+
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+
+	// range on slices or arrays will return index and actual number as two param
+	for i, num := range nums {
+		fmt.Println("i", i, "num:", num)
+	}
+
+	fmt.Println("Sum :", sum)
+
+	m := map[string]int{"key1": 1, "key2": 2}
+
+	// range on map returns key and value as two params
+	for k, v := range m {
+		fmt.Println("key:", k, "value:", v)
+	}
+
+	// key will returned if u r using only one param
+	for k := range m {
+		fmt.Println(k)
+	}
+
+	//range on strings iterates over Unicode code points.
+	//The first value is the starting byte index of the rune and the second the rune itself
+	for i, c := range "Golang" {
+		fmt.Println("i:", i, "c", c)
+	}
+
+	for i, c := range "Golang" {
+		fmt.Println("i:", i, "c", string(c))
+	}
+}
